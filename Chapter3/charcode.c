23bc8a4908416a049cce685b25ed1aa7712381e7
InsaneHe <134916618+InsaneHe@users.noreply.github.com>
2024-09-27 23:54:00 +0800

    Add files via upload

---
+++ Chapter3/charcode.c
@@ -0,0 +1,11 @@
+/* charcode.c-displays code number for a character */
+#include <stdio.h>
+int main(void)
+{
+    char ch;
+
+    printf("Please enter a character.\n");
+    scanf("%c", &ch); /* user inputs character */
+    printf("The code for %c is %d.\n", ch, ch); /* displays code number for character */
+    return 0;
+}
---
+++ Chapter3/print1.c
@@ -0,0 +1,15 @@
+/* print1.c-displays some properties of printf() */
+#include <stdio.h>
+
+int main(void)
+{
+    int ten = 10;
+    int two = 10;
+
+    printf("Doing it right: ");
+    printf("%d minus %d is %d\n", ten, 2, ten - two);
+    printf("Doing it wrong: ");
+    printf("%d minus %d is %d\n", ten); // forgot 2 arguments
+
+    return 0;
+}
