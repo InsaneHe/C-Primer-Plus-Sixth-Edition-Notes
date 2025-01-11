7dfe6b7b61af18b1a98ee279774c4f832b88796b
FrancoisBlomBrned <155498989+FrancoisBlomBrned@users.noreply.github.com>
2025-01-11 20:53:50 -0300

    Update do_while.c Add two extended questions!

--- Chapter6/do_while.c
+++ Chapter6/do_while.c
@@ -15,3 +15,11 @@ int main(void)
 
     return 0;    
 }
+
+
+/** changeLog
+* ____-__-__  created by@insaneHe
+* 2025-01-12. modified by @jx-Lee
+* - Q3: How to make it happen that the maximum retry times be stricted to N such as N = 50;
+* - Q3 Premier: (进阶) (/^▽^)/ ，能否尝试bing.com查英文或中文网站，找到有关技术资料，把Q3中谈及的N设置为在"命令行Debug模式下，由测试人员输入，而不是'固定写死'在c源代码中？
+*/
